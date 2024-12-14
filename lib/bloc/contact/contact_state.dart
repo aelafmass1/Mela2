0de04a53e12a7d01883f1481e91febc6c93a69d5
actuponit <imo60067035@gmail.com>
2024-12-14 09:24:49 +0300

    refactor: extend ContactState with Equatable for improved state comparison

--- lib/bloc/contact/contact_state.dart
+++ lib/bloc/contact/contact_state.dart
@@ -6,39 +6,42 @@
 /// [ContactSuccess] is the state when the contact data is successfully loaded.
 part of 'contact_bloc.dart';
 
-sealed class ContactState {
+sealed class ContactState extends Equatable {
   final List<String> remoteContacts;
   final List<Contact> localContacs;
 
-  ContactState({this.remoteContacts = const [], this.localContacs = const []});
+  const ContactState(
+      {this.remoteContacts = const [], this.localContacs = const []});
+
+  @override
+  List<Object> get props => [remoteContacts, localContacs];
 }
 
 final class ContactInitial extends ContactState {
-  ContactInitial({super.remoteContacts, super.localContacs});
+  const ContactInitial({super.remoteContacts, super.localContacs});
 }
 
 final class ContactLoading extends ContactState {
-  ContactLoading({super.remoteContacts, super.localContacs});
+  const ContactLoading({super.remoteContacts, super.localContacs});
 }
 
 final class ContactFail extends ContactState {
   final String message;
 
-  ContactFail(
+  const ContactFail(
       {required this.message, super.remoteContacts, super.localContacs});
-}
 
-final class ContactSuccess extends ContactState {
-  ContactSuccess({required super.remoteContacts, super.localContacs});
+  @override
+  List<Object> get props => [message];
 }
 
 final class ContactFilterLoading extends ContactState {
-  ContactFilterLoading({super.remoteContacts, super.localContacs});
+  const ContactFilterLoading({super.remoteContacts, super.localContacs});
 }
 
 final class ContactFilterSuccess extends ContactState {
   final List<ContactStatusModel> filteredContacts;
-  ContactFilterSuccess(
+  const ContactFilterSuccess(
       {required this.filteredContacts,
       super.remoteContacts,
       super.localContacs});
@@ -46,6 +49,6 @@ final class ContactFilterSuccess extends ContactState {
 
 final class ContactFilterFailed extends ContactState {
   final String message;
-  ContactFilterFailed(
+  const ContactFilterFailed(
       {required this.message, super.remoteContacts, super.localContacs});
 }
