08f6fae3e93ada0d336cd0ff50ad7e88527eb1f2
actuponit <imo60067035@gmail.com>
2024-12-14 09:23:24 +0300

    added mocks

---
+++ test/helper/helper.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+import 'package:transaction_mobile_app/data/repository/contact_repository.dart';
+
+// Generate mock classes
+
+@GenerateMocks([ContactRepository])
+void main() {}
