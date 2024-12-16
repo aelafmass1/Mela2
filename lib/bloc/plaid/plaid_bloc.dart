29a25d54037851599b1797ad0bd1a3fe1a030299
actuponit <imo60067035@gmail.com>
2024-12-17 01:47:05 +0300

    refactor: remove ExchangePublicToken event and associated logic from PlaidBloc

--- lib/bloc/plaid/plaid_bloc.dart
+++ lib/bloc/plaid/plaid_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plaid_flutter/plaid_flutter.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:transaction_mobile_app/data/models/payment_card_model.dart';
 import 'package:transaction_mobile_app/data/repository/plaid_repository.dart';
@@ -16,7 +17,6 @@ class PlaidBloc extends Bloc<PlaidEvent, PlaidState> {
   final PlaidRepository repository;
   PlaidBloc({required this.repository}) : super(PlaidInitial()) {
     on<CreateLinkToken>(_onCreateLinkToken);
-    on<ExchangePublicToken>(_onExchangePublicToken);
     on<AddBankAccount>(_onAddBankAccount);
   }
   _onAddBankAccount(AddBankAccount event, Emitter emit) async {
@@ -30,9 +30,7 @@ class PlaidBloc extends Bloc<PlaidEvent, PlaidState> {
         if (res.containsKey('error')) {
           return emit(AddBankAccountFail(reason: res['error']));
         }
-        emit(AddBankAccountSuccess(
-            paymentCard:
-                PaymentCardModel.fromMap(res as Map<String, dynamic>)));
+        emit(AddBankAccountSuccess());
       }
     } on ServerException catch (error, stackTrace) {
       emit(AddBankAccountFail(reason: error.message));
@@ -46,31 +44,6 @@ class PlaidBloc extends Bloc<PlaidEvent, PlaidState> {
     }
   }
 
-  _onExchangePublicToken(ExchangePublicToken event, Emitter emit) async {
-    try {
-      if (state is! PlaidPublicTokenLoading) {
-        emit(PlaidPublicTokenLoading());
-        final token = await getToken();
-
-        final res =
-            await repository.exchangePublicToken(token!, event.publicToken);
-        if (res.containsKey('error')) {
-          return emit(PlaidPublicTokenFail(reason: res['error']));
-        }
-        emit(PlaidPublicTokenSuccess());
-      }
-    } on ServerException catch (error, stackTrace) {
-      emit(PlaidPublicTokenFail(reason: error.message));
-      await Sentry.captureException(
-        error,
-        stackTrace: stackTrace,
-      );
-    } catch (error) {
-      log(error.toString());
-      emit(PlaidPublicTokenFail(reason: error.toString()));
-    }
-  }
-
   _onCreateLinkToken(CreateLinkToken event, Emitter emit) async {
     try {
       if (state is! PlaidLinkTokenLoading) {
@@ -81,7 +54,14 @@ class PlaidBloc extends Bloc<PlaidEvent, PlaidState> {
         if (res.containsKey('error')) {
           return emit(PlaidLinkTokenFail(reason: res['error']));
         }
-        emit(PlaidLinkTokenSuccess(linkToken: res['linkToken']));
+        var linkToken = res['linkToken'];
+        emit(PlaidLinkTokenSuccess(linkToken: linkToken));
+
+        LinkTokenConfiguration(
+          token: linkToken,
+        );
+        await PlaidLink.create(configuration: linkToken!);
+        await PlaidLink.open();
       }
     } on ServerException catch (error, stackTrace) {
       emit(PlaidLinkTokenFail(reason: error.message));
